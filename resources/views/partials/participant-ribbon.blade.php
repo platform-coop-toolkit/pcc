278a46dcd6bbed87714d7d5029bd028f491664e4
Ned Zimmerman <ned@bight.ca>
2019-05-28 10:33:31 -0300

    Fix localization

--- resources/views/partials/participant-ribbon.blade.php
+++ resources/views/partials/participant-ribbon.blade.php
@@ -4,22 +4,22 @@
       <li class="menu-item">
         <a @if(!$event_view) rel="current" @endif href="@permalink">
         @if($event_type === 'conference')
-          {{ __('Conference', 'platformcoop') }}
+          {{ __('Conference', 'pcc') }}
         @else
-          {{ __('Event', 'platformcoop') }}
+          {{ __('Event', 'pcc') }}
         @endif
         </a>
       </li>
       <li class="menu-item"><a @if($event_view === 'program')
           rel="current"
-          @endif href="{{ get_permalink() }}program/">{{ __('Program', 'platformcoop') }}</a></li>
+          @endif href="{{ get_permalink() }}program/">{{ __('Program', 'pcc') }}</a></li>
       <li class="menu-item"><a @if($event_view === 'participants')
           rel="current"
           @endif @if($event_view === 'participant')
           class="parent"
-          @endif href="{{ get_permalink() }}participants/">{{ __('Participants', 'platformcoop') }}</a></li>
+          @endif href="{{ get_permalink() }}participants/">{{ __('Participants', 'pcc') }}</a></li>
       @if(SinglePccEvent::registrationLink())
-      <li class="wp-block-button is-style-primary"><a class="wp-block-button__link" href="{{ SinglePccEvent::registrationLink() }}">{{ __('Register Now', 'platformcoop') }}</a></li>
+      <li class="wp-block-button is-style-primary"><a class="wp-block-button__link" href="{{ SinglePccEvent::registrationLink() }}">{{ __('Register Now', 'pcc') }}</a></li>
       @endif
     </ul>
   </nav>
