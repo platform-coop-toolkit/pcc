909e7aa77fd2451960344ee2066869bddbb4033c
Ned Zimmerman <ned@bight.ca>
2019-06-20 10:31:51 -0300

    Add participant image placeholders; pin ribbon on scroll

--- resources/assets/scripts/routes/event.js
+++ resources/assets/scripts/routes/event.js
@@ -12,6 +12,8 @@ export default {
     const scrollNext = document.getElementById('scroll-next');
     const ribbonNav = document.querySelector('.ribbon nav')
     const ribbonNavContents = ribbonNav.querySelector('ul');
+    const ribbon = document.querySelector('.ribbon');
+    const sticky = ribbon.offsetTop;
 
     ribbonNav.setAttribute('data-overflowing', determineOverflow(ribbonNavContents, ribbonNav));
 
@@ -22,11 +24,25 @@ export default {
       ribbonNav.setAttribute('data-overflowing', determineOverflow(ribbonNavContents, ribbonNav));
     }
 
+    window.addEventListener('scroll', function() {
+      if (!ticking) {
+        window.requestAnimationFrame(function() {
+          if (window.pageYOffset > sticky) {
+            ribbon.classList.add('sticky');
+          } else {
+            ribbon.classList.remove('sticky');
+          }
+          ticking = false;
+        });
+      }
+      ticking = true;
+    });
+
     ribbonNav.addEventListener('scroll', function() {
         if (!ticking) {
             window.requestAnimationFrame(function() {
               updateOverflow();
-                ticking = false;
+              ticking = false;
             });
         }
         ticking = true;
@@ -76,7 +92,6 @@ export default {
 
         // Now we know how much space we have available to scroll
         let availableScrollRight = Math.floor(navBarRightEdge - navBarScrollerRightEdge);
-        console.log(`${navBarRightEdge} ${navBarScrollerRightEdge} ${availableScrollRight}`);
         // If the space available is less than two lots of our desired distance, just move the whole amount
         // otherwise, move by the amount in the settings
         if (availableScrollRight < SETTINGS.navBarTravelDistance * 2) {
@@ -97,7 +112,6 @@ export default {
   ribbonNavContents.addEventListener(
     'transitionend',
     function() {
-      console.log('transition end');
         // get the value of the transform, apply that to the current scroll position (so get the scroll pos first) and then remove the transform
         var styleOfTransform = window.getComputedStyle(ribbonNavContents, null);
         var tr = styleOfTransform.getPropertyValue('-webkit-transform') || styleOfTransform.getPropertyValue('transform');
@@ -123,7 +137,6 @@ export default {
     var contentMetrics = content.getBoundingClientRect();
     var contentMetricsRight = Math.floor(contentMetrics.right);
     var contentMetricsLeft = Math.floor(contentMetrics.left);
-    console.log( `${containerMetricsLeft} > ${contentMetricsLeft} && ${containerMetricsRight} < ${contentMetricsRight}`)
     if (containerMetricsLeft > contentMetricsLeft && containerMetricsRight < contentMetricsRight) {
         return 'both';
     } else if (contentMetricsLeft < containerMetricsLeft) {
--- resources/views/partials/event-participant.blade.php
+++ resources/views/partials/event-participant.blade.php
@@ -1,9 +1,11 @@
 <li class="participant">
-  @if(!empty($participant['headshot']))
   <figure class="participant__image">
-    {!! wp_get_attachment_image($participant['headshot'], 'person-desktop') !!}
+    @if(!empty($participant['headshot']))
+      {!! wp_get_attachment_image($participant['headshot'], 'person-desktop') !!}
+    @else
+      <div role="img" style="background-color: #30cfc9; height: 100%;"></div>
+    @endif
   </figure>
-  @endif
   <div class="participant__details">
     <p class="participant__name"><a href="{{ get_permalink() }}participants/{{ $participant['slug'] }}/">{{ $participant['name'] }}</a></p>
     <p class="participant__title">{{ $participant['title'] }}</p>
