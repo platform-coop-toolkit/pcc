c3e02592fc2922ff0fa6413254ea16d8f1ab7c07
Ned Zimmerman <ned@bight.ca>
2019-07-02 15:42:54 -0300

    Revert "Support HTTPS in Percy"
    3a9374709ea78ef509ff82a374e99a328ad9a28f

--- snapshots.js
+++ snapshots.js
@@ -1,22 +1,18 @@
 const PercyScript = require('@percy/script');
 
-const options = {
-  ignoreHTTPSErrors: true,
-};
-
 PercyScript.run(async (page, percySnapshot) => {
-  await page.goto('https://platform.coop.test/');
+  await page.goto('http://platform.coop.test/');
   await percySnapshot('home');
-  await page.goto('https://platform.coop.test/contact-us/');
+  await page.goto('http://platform.coop.test/contact-us/');
   await percySnapshot('contact');
-  await page.goto('https://platform.coop.test/open-access-and-privacy-policy/');
+  await page.goto('http://platform.coop.test/open-access-and-privacy-policy/');
   await percySnapshot('privacy');
-  await page.goto('https://platform.coop.test/about/');
+  await page.goto('http://platform.coop.test/about/');
   await percySnapshot('about');
-  await page.goto('https://platform.coop.test/about/vision-and-advantages/');
+  await page.goto('http://platform.coop.test/about/vision-and-advantages/');
   await percySnapshot('vision');
-  await page.goto('https://platform.coop.test/about/benefits/about/benefits/member-owners/');
+  await page.goto('http://platform.coop.test/about/benefits/about/benefits/member-owners/');
   await percySnapshot('benefits');
-  await page.goto('https://platform.coop.test/events/conference-2017/');
+  await page.goto('http://platform.coop.test/events/conference-2017/');
   await percySnapshot('event');
-}, options);
+});
