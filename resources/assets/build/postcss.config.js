e5940321ff7188c048eb297ab8457fc099d45d84
Ned Zimmerman <ned@bight.ca>
2019-07-04 15:36:57 -0300

    WIP

--- resources/assets/build/postcss.config.js
+++ resources/assets/build/postcss.config.js
@@ -7,9 +7,16 @@ module.exports = ({ file, options }) => {
   return {
     parser: options.enabled.optimize ? 'postcss-safe-parser' : undefined,
     plugins: {
+      'postcss-import': {},
+      'postcss-partial-import': {},
+      'postcss-custom-media': {},
+      'postcss-custom-properties': {
+        importFrom: './resources/assets/styles/common/_properties.css',
+        preserve: false,
+      },
+      'postcss-calc': {},
       autoprefixer: true,
       cssnano: options.enabled.optimize ? cssnanoConfig : false,
-      'postcss-calc': {},
     },
   };
 };
