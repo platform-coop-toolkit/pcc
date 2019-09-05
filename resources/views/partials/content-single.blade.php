dd577d91f3dc4cb39b8d7a3626efbbd979142d14
Ned Zimmerman <ned@bight.ca>
2019-09-04 19:47:09 -0600

    Desktop single view

--- resources/views/partials/content-single.blade.php
+++ resources/views/partials/content-single.blade.php
@@ -1,19 +1,21 @@
 <article @php post_class('container') @endphp>
   <header>
     <h1 class="entry-title">{!! App::title() !!}</h1>
-    @include('partials/entry-meta')
+  </header>
+  @include('partials/entry-meta')
+  <div class="content" id="content">
     @if(has_post_thumbnail())
       <figure>{!! get_the_post_thumbnail($post, 'original') !!}</figure>
     @endif
-  </header>
-  <div class="content" id="content">
     @content
   </div>
   <footer>
+    @if(get_the_tags())
     <div class="tags">
       <p class="tags__label">{{ __('Tags', 'pcc') }}</p>
       {!! Single::tags() !!}
     </div>
+    @endif
   </footer>
   @php comments_template('/partials/comments.blade.php') @endphp
 </article>
