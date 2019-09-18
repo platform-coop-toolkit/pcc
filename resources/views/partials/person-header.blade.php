4b15dd388d328846793e249075d0fc98911d5111
Ned Zimmerman <ned@bight.ca>
2019-09-17 21:22:08 -0600

    Move people links below the bio as per designs (#129)
    * Move people links below the bio as per designs.
    
    * Fix coding standards

--- resources/views/partials/person-header.blade.php
+++ resources/views/partials/person-header.blade.php
@@ -10,17 +10,10 @@
         @include('partials/breadcrumb')
       @endif
       <h1>{!! App::title() !!}</h1>
-      @if($participant_data['title'] && $participant_data['organization'] && $participant_data['organization_link'])
-      <p class="title">{{ $participant_data['title'] }}, <a href="{{ $participant_data['organization_link'] }}">{{ $participant_data['organization'] }}</a>
-      </p>
-      @elseif($participant_data['title'] && $participant_data['organization'])
-      <p class="title">{{ $participant_data['title'] }}, {{ $participant_data['organization'] }}
-      </p>
+      @if($participant_data['title'] && $participant_data['organization'])
+      <p class="title">{{ $participant_data['title'] }}, {{ $participant_data['organization'] }}</p>
       @elseif($participant_data['title'])
       <p class="title">{{ $participant_data['title'] }}</p>
-      @endif
-      @if($participant_data['twitter_username'])
-      <p class="twitter"><a href="https://twitter.com/{{ str_replace('@', '', $participant_data['twitter_username']) }}">{{ $participant_data['twitter_username'] }}</a></p>
       @endif
     </div>
     @if(has_post_thumbnail())
