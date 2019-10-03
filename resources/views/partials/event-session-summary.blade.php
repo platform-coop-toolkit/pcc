a813684b18a65064a0a53e0eb2ab1d9a4dcf3f69
Ned Zimmerman <ned@bight.ca>
2019-10-03 09:49:22 -0600

    Fix #151

--- resources/views/partials/event-session-summary.blade.php
+++ resources/views/partials/event-session-summary.blade.php
@@ -5,7 +5,7 @@
         <a href="@permalink($session->ID)">@title($session->ID) @svg('chevron-right', ['aria-hidden' => 'true', 'viewBox' => '0 0 5.886 9.8'])</a>
       </p>
       @if(SinglePccEvent::sessionVenue($session->ID))
-      <p class="session__location">@svg('location', ['aria-hidden' => true, 'viewBox' => '0 0 10.581 15.183']) {{ SinglePccEvent::sessionVenue($session->ID) }}</p>
+      <p class="session__location">@svg('location', ['aria-hidden' => true, 'viewBox' => '0 0 10.581 15.183']) {!! SinglePccEvent::sessionVenue($session->ID) !!}</p>
       @endif
       @if(SinglePccEvent::sessionParticipants($session->ID))
       <p class="session__participants">{{ implode(', ', SinglePccEvent::sessionParticipants($session->ID)) }}</p>
