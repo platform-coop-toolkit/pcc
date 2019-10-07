33fcd6d1a3872f6e2663e4895cebc8b1961317c4
Ned Zimmerman <ned@bight.ca>
2019-10-07 15:40:34 -0600

    Fix titles on program grid

--- resources/views/partials/event-session-summary.blade.php
+++ resources/views/partials/event-session-summary.blade.php
@@ -2,7 +2,7 @@
     <time class="session__time" datetime="{{ strftime('%FT%T', $session->pcc_event_start) }}">{{ strftime('%l:%M%p', $session->pcc_event_start) }}</time>
     <div class="session__info">
       <p class="session__title">
-        <a href="@permalink($session->ID)">@title($session->ID) @svg('chevron-right', ['aria-hidden' => 'true', 'viewBox' => '0 0 5.886 9.8'])</a>
+        <a href="@permalink($session->ID)">@title($session->ID)&nbsp;@svg('chevron-right', ['aria-hidden' => 'true', 'viewBox' => '0 0 5.886 9.8'])</a>
       </p>
       @if(SinglePccEvent::sessionVenue($session->ID))
       <p class="session__location">@svg('location', ['aria-hidden' => true, 'viewBox' => '0 0 10.581 15.183']) {!! SinglePccEvent::sessionVenue($session->ID) !!}</p>
