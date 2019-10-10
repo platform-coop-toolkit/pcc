9c7d0e149891aa51b1972441c8ff7d7019457030
Ned Zimmerman <ned@bight.ca>
2019-10-10 09:27:40 -0600

    Only show credited photos on photo credits page (#167)
    - Fixes #166

--- app/Controllers/Page.php
+++ app/Controllers/Page.php
@@ -11,70 +11,80 @@ class Page extends Controller
         if (basename(get_page_template()) !== 'page-photo-credits.blade.php') {
             return;
         }
-        $photos = get_posts(['post_type' => 'attachment', 'numberposts' => '-1']);
+        $photos = get_posts([
+            'post_type' => 'attachment',
+            'numberposts' => '-1',
+            'order' => 'asc',
+            'meta_query' => [
+                'relation' => 'OR',
+                [
+                    'key'     => 'pcc_attachment_creator_name',
+                    'compare' => 'EXISTS',
+                ],
+                [
+                    'key'     => 'pcc_attachment_organization_name',
+                    'compare' => 'EXISTS',
+                ],
+            ],
+        ]);
         foreach ($photos as $k => $photo) {
-            if (get_post_type($photo->post_parent) === 'pcc-event') {
-                // Don't include event banners.
-                unset($photos[$k]);
-            } else {
-                $pcc_attachment_creator_name = get_post_meta($photo->ID, 'pcc_attachment_creator_name', true);
-                $pcc_attachment_creator_link = get_post_meta($photo->ID, 'pcc_attachment_creator_link', true);
-                $pcc_attachment_organization_name = get_post_meta($photo->ID, 'pcc_attachment_organization_name', true);
-                $pcc_attachment_organization_link = get_post_meta($photo->ID, 'pcc_attachment_organization_link', true);
-                if ($pcc_attachment_creator_name &&
-                    $pcc_attachment_organization_name) {
-                    if ($pcc_attachment_creator_link) {
-                        $creator = sprintf(
-                            '<a href="%1$s">%2$s</a>',
-                            $pcc_attachment_creator_link,
-                            $pcc_attachment_creator_name
-                        );
-                    } else {
-                        $creator = $pcc_attachment_creator_name;
-                    }
-                    if ($pcc_attachment_organization_link) {
-                        $organization = sprintf(
-                            '<a href="%1$s">%2$s</a>',
-                            $pcc_attachment_organization_link,
-                            $pcc_attachment_organization_name
-                        );
-                    } else {
-                        $organization = $pcc_attachment_organization_name;
-                    }
-                    $photo->credit = sprintf(
-                        'Photo by %1$s for %2$s',
-                        $creator,
-                        $organization
+            $pcc_attachment_creator_name = get_post_meta($photo->ID, 'pcc_attachment_creator_name', true);
+            $pcc_attachment_creator_link = get_post_meta($photo->ID, 'pcc_attachment_creator_link', true);
+            $pcc_attachment_organization_name = get_post_meta($photo->ID, 'pcc_attachment_organization_name', true);
+            $pcc_attachment_organization_link = get_post_meta($photo->ID, 'pcc_attachment_organization_link', true);
+            if ($pcc_attachment_creator_name &&
+                $pcc_attachment_organization_name) {
+                if ($pcc_attachment_creator_link) {
+                    $creator = sprintf(
+                        '<a href="%1$s">%2$s</a>',
+                        $pcc_attachment_creator_link,
+                        $pcc_attachment_creator_name
                     );
-                } elseif ($pcc_attachment_creator_name) {
-                    if ($pcc_attachment_creator_link) {
-                        $creator = sprintf(
-                            '<a href="%1$s">%2$s</a>',
-                            $pcc_attachment_creator_link,
-                            $pcc_attachment_creator_name
-                        );
-                    } else {
-                        $creator = $pcc_attachment_creator_name;
-                    }
-                    $photo->credit = sprintf(
-                        'Photo by %1$s',
-                        $creator
-                    );
-                } elseif ($pcc_attachment_organization_name) {
-                    if ($pcc_attachment_organization_link) {
-                        $organization = sprintf(
-                            '<a href="%1$s">%2$s</a>',
-                            $pcc_attachment_organization_link,
-                            $pcc_attachment_organization_name
-                        );
-                    } else {
-                        $organization = $pcc_attachment_organization_name;
-                    }
-                    $photo->credit = sprintf(
-                        'Photo by %1$s',
-                        $organization
-                    );
+                } else {
+                    $creator = $pcc_attachment_creator_name;
                 }
+                if ($pcc_attachment_organization_link) {
+                    $organization = sprintf(
+                        '<a href="%1$s">%2$s</a>',
+                        $pcc_attachment_organization_link,
+                        $pcc_attachment_organization_name
+                    );
+                } else {
+                    $organization = $pcc_attachment_organization_name;
+                }
+                $photo->credit = sprintf(
+                    'Photo by %1$s for %2$s',
+                    $creator,
+                    $organization
+                );
+            } elseif ($pcc_attachment_creator_name) {
+                if ($pcc_attachment_creator_link) {
+                    $creator = sprintf(
+                        '<a href="%1$s">%2$s</a>',
+                        $pcc_attachment_creator_link,
+                        $pcc_attachment_creator_name
+                    );
+                } else {
+                    $creator = $pcc_attachment_creator_name;
+                }
+                $photo->credit = sprintf(
+                    'Photo by %1$s',
+                    $creator
+                );
+            } elseif ($pcc_attachment_organization_name) {
+                if ($pcc_attachment_organization_link) {
+                    $organization = sprintf(
+                        '<a href="%1$s">%2$s</a>',
+                        $pcc_attachment_organization_link,
+                        $pcc_attachment_organization_name
+                    );
+                } else {
+                    $organization = $pcc_attachment_organization_name;
+                }
+                $photo->credit = sprintf(
+                    'Photo by %1$s',
+                    $organization
+                );
             }
         }
         return $photos;
