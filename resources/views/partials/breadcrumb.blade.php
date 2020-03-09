2a95a99c0eef7e20bda8216999ffdb15e6952841
Ned Zimmerman <ned@bight.dev>
2020-03-09 15:16:37 -0600

    fix: improve breadcrumb layout (fixes #55)

--- resources/views/partials/breadcrumb.blade.php
+++ resources/views/partials/breadcrumb.blade.php
@@ -1,3 +1,8 @@
 <p class="breadcrumb">
-  <a href="{{ $breadcrumb['url'] }}">{{ $breadcrumb['label'] }}</a>
+  <a href="{{ $breadcrumb['url'] }}">
+    @if($breadcrumb['hide_back_to'])
+      <span class="screen-reader-text">{{ __('Back to', 'pcc') }} </span>{!! $breadcrumb['label'] !!}
+    @else
+    {!! sprintf(__('Back to %s', 'pcc'), $breadcrumb['label']) !!}
+    @endif</a>
 </p>
