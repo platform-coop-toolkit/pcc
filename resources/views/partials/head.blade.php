2c89c12bf6a0bf6c69e284719f4b640a5f01485b
Ned Zimmerman <ned@bight.dev>
2020-04-14 13:29:48 -0600

    feat: add SVG version

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,7 +2,8 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="icon" href="{{ App\asset_path('images/favicon.png') }}">
+  <link rel="shortcut icon" href="{{ App\asset_path('images/favicon.png') }}" type="image/png">
+  <link rel="shortcut icon" href="{{ App\asset_path('images/favicon.svg') }}" type="image/svg+xml">
   <script>(function(html){html.className = html.className.replace(/\bno-js\b/,'js')})(document.documentElement);</script>
   @if(is_page_template('views/page-contact-us.blade.php') || is_page('contact-us'))
     @if(function_exists('wpcf7_enqueue_scripts'))
