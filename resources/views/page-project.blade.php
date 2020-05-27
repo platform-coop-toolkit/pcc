3f9cb2c4f4deda6c6caaa0afb51a26c8a1551080
Elizabeth Mitchell <elizabeth@sassafras.coop>
2020-05-27 15:16:52 -0400

    10517: Add project template

---
+++ resources/views/page-project.blade.php
@@ -0,0 +1,11 @@
+{{--
+  Template Name: Project Page
+--}}
+
+@extends('layouts.app')
+
+@section('content')
+  @while(have_posts()) @php the_post() @endphp
+    @include('partials.page-header')
+  @endwhile
+@endsection
