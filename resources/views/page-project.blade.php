87d58a63d7c32e4d94baedc113387864968b0834
Elizabeth Mitchell <elizabeth@sassafras.coop>
2020-05-27 17:00:08 -0400

    10517: Add sections to template

--- resources/views/page-project.blade.php
+++ resources/views/page-project.blade.php
@@ -6,6 +6,17 @@
 
 @section('content')
   @while(have_posts()) @php the_post() @endphp
+    <div>
     @include('partials.page-header')
+    <section class="about-project section">
+      <h2 class="section-heading">About</h2>
+      <p>@php the_content() @endphp</p>
+    </section>
+    <section class="featured-briefs section">
+      <h2 class="section-heading">Featured Briefs</h2>
+    </section>
+    <section class="people section">
+      <h2 class="section-heading">People</h2>
+    </section>
   @endwhile
 @endsection
