4234a01c687e0b3154977b3d880309d69d6bf2a7
Elizabeth Mitchell <elizabeth@sassafras.coop>
2020-06-09 07:37:09 -0400

    10676: Apply project styles across project pages

--- resources/views/page-project-research.blade.php
+++ resources/views/page-project-research.blade.php
@@ -4,13 +4,14 @@
 
 @extends('layouts.app')
 
-<div class="project">
-  @section('content')
-    @while(have_posts()) @php the_post() @endphp
+
+@section('content')
+  @while(have_posts()) @php the_post() @endphp
+    <div class="project">
       @include('partials.project-header')
       <div class="project-content">
         @include('partials.project-breadcrumbs')
       </div>
-    @endwhile
-  @endsection
-</div>
+    </div>
+  @endwhile
+@endsection
