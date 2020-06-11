e3b9c81c09a4aca81dd22b0642bc57fdcfec95aa
Elizabeth Mitchell <elizabeth@sassafras.coop>
2020-06-11 13:36:25 -0400

    10840: Adjust classes to apply styles to project post

--- resources/views/single-pcc-project.blade.php
+++ resources/views/single-pcc-project.blade.php
@@ -6,22 +6,24 @@
 
 @section('content')
   @while(have_posts()) @php the_post() @endphp
-    @include('partials.project-header')
-    <div class="project-content">
-      @include('partials.project-breadcrumbs')
-      <section class="about-project section">
-        <div class="heading-with-line">
-          <h2 class="section-heading">About</h2>
-          <hr>
-        </div>
-        <p>@php the_content() @endphp</p>
-      </section>
-      <section class="featured-briefs section">
-        <h2 class="section-heading">Featured Briefs</h2>
-      </section>
-      <section class="people section">
-        <h2 class="section-heading">People</h2>
-      </section>
+    <div class="pcc-project">
+      @include('partials.project-header')
+      <div class="project-content">
+        @include('partials.project-breadcrumbs')
+        <section class="about-project section">
+          <div class="heading-with-line">
+            <h2 class="section-heading">About</h2>
+            <hr>
+          </div>
+          <p>@php the_content() @endphp</p>
+        </section>
+        <section class="featured-briefs section">
+          <h2 class="section-heading">Featured Briefs</h2>
+        </section>
+        <section class="people section">
+          <h2 class="section-heading">People</h2>
+        </section>
+      </div>
     </div>
   @endwhile
 @endsection
