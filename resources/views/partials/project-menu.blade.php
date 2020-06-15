b01bc5122b2f2adee522e12138e553ad61dade07
Elizabeth Mitchell <elizabeth@sassafras.coop>
2020-06-15 11:53:48 -0400

    10824: Use ribbon menu styles inside project

--- resources/views/partials/project-menu.blade.php
+++ resources/views/partials/project-menu.blade.php
@@ -1,4 +1,4 @@
-<div class="project-menu">
+<!-- <div class="pcc-ribbon ribbon project-menu">
   <nav>
     <ul>
       <li class="active">About - Main Project Link</li>
@@ -8,5 +8,24 @@
       <li>Test</li>
       <li>Test</li>
     </ul>
+  </nav>
+</div> -->
+
+
+<div class="pcc-ribbon ribbon project-menu">
+  <nav>
+    <button id="scroll-previous" class="scroll-left">
+        @svg('chevron-left', ['aria-hidden' => 'true', 'height' => '12', 'width' => '7.21'])
+      <span class="screen-reader-text">{{ __('Scroll to the left', 'pcc') }}</span>
+    </button>
+    <ul>
+      <li class="menu-item active"><a>About - Main Project Link</a></li>
+      <li class="menu-item"><a>Research</a></li>
+      <li class="menu-item menu-item--last"><a>Test</a></li>
+    </ul>
+    <button id="scroll-next" class="scroll-right">
+        @svg('chevron-right', ['aria-hidden' => 'true', 'height' => '12', 'width' => '7.21'])
+      <span class="screen-reader-text">{{ __('Scroll to the right', 'pcc') }}</span>
+    </button>
   </nav>
 </div>
