e3fccacb2d33760f3a452b3e6daef277f736f9bf
Elizabeth Mitchell <elizabeth@sassafras.coop>
2020-06-17 14:28:58 -0400

    10676: Add temporary title

--- resources/views/partials/project-header.blade.php
+++ resources/views/partials/project-header.blade.php
@@ -2,5 +2,6 @@
   <figure class="project-header-media">
       @thumbnail('banner')
   </figure>
-  <h1>{!! App::title() !!}</h1>
+  <!-- <h1>{!! App::title() !!}</h1> -->
+  <h1>India: Platform Coop Research</h1>
 </div>
