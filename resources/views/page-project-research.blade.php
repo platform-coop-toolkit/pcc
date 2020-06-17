7974eab6363290c38d65821cdf41069cfda803e4
Elizabeth Mitchell <elizabeth@sassafras.coop>
2020-06-17 08:07:14 -0400

    10676: Build out research index template

--- resources/views/page-project-research.blade.php
+++ resources/views/page-project-research.blade.php
@@ -12,16 +12,24 @@
       <section class="project-content">
         @include('partials.project-breadcrumbs')
         <h1>{!! App::title() !!}</h1>
-        <div class="map-container"></div>
+        <div class="map-container">
+          @php get_post_custom( $map_code ) @endphp
+        </div>
+
+        <div>@php the_content() @endphp</div>
         <section class="section research-locations">
-          <h2 class="section-heading">List of Places</h2>
-          <p class="section-description">Some text about the list of places.</p>
-          <ul class="location-list">
-            <li>Location 1</li>
-            <li>Location 2</li>
-            <li>Location 3</li>
-            <li>Location 4</li>
-          </ul>
+          <div class="section-info">
+            <h2 class="section-heading">List of Places</h2>
+            <p class="section-description">Some text about the list of places.</p>
+          </div>
+          <div class="section-content">
+            <ul class="location-list">
+              <li>Location 1</li>
+              <li>Location 2</li>
+              <li>Location 3</li>
+              <li>Location 4</li>
+            </ul>
+          </div>
         </section>
       </section>
     </div>
