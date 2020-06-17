8748ab443cb9a81cc635ce24faaa3f7afe4252ae
Elizabeth Mitchell <elizabeth@sassafras.coop>
2020-06-17 09:57:08 -0400

    10676: Remove reference to template that does not exist on branch

--- resources/views/single-pcc-project.blade.php
+++ resources/views/single-pcc-project.blade.php
@@ -8,7 +8,6 @@
   @while(have_posts()) @php the_post() @endphp
     <div class="pcc-project">
       @include('partials.project-header')
-      <!-- @include('partials.project-menu') -->
       <div class="project-content side-margins">
         @include('partials.project-breadcrumbs')
         <section class="about-project section">
