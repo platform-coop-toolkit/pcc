77bda8627ce8130017d3dda0ba8b725e8fcf49f4
Jill Dimond <jill@sassafras.coop>
2020-07-06 11:07:17 -0400

    additional code review changes

--- resources/views/partials/project-header.blade.php
+++ resources/views/partials/project-header.blade.php
@@ -1,6 +1,6 @@
 <div class="project-header">
   <figure class="project-header-media">
-    {!! get_the_post_thumbnail(SinglePccProject::rootPage($id, $post), 'banner') !!}
+    {!! get_the_post_thumbnail(SinglePccProject::rootPage(get_the_id(), $post), 'banner') !!}
   </figure>
   <div class="title">
     <h1>{{ SinglePccProject::projectTitle() }}</h1>
